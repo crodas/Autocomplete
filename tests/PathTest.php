52608db443712db2e81161be1141e5f9f354852e
César D. Rodas <crodas@php.net>
2013-08-31 05:41:09 -0400

    First commit

---
+++ tests/PathTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use crodas\Autocomplete\Autocomplete;
+
+class PathTest extends \phpunit_framework_testcase
+{
+    public function testLearn()
+    {
+        global $pdo;
+        $ac = new Autocomplete(new DB($pdo));
+        $pdo->beginTransaction();
+        foreach(file(__DIR__ . '/data/brit-a-z.txt') as $word) {
+            $ac->index($word, 100-strlen($word));
+        }
+        $ac->index('Something pretty large', 999);
+        $pdo->commit();
+    }
+
+    /** @dependsOn testLearn */
+    public function testMultiword()
+    {
+        global $pdo;
+        $ac = new Autocomplete(new DB($pdo));
+        $words = $ac->suggest('lar');
+        $this->assertEquals($words[0]['word'], 'something pretty large');
+    }
+
+    /** @dependsOn testLearn */
+    public function testSpeed() 
+    {
+        global $pdo;
+        $ac = new Autocomplete(new DB($pdo));
+        $time = microtime(true);
+        $this->assertEquals($ac->suggest('Fath'), $ac->suggest('FATH'));
+        $this->assertEquals([], $ac->suggest('xxxx'));
+        $this->assertTrue(microtime(true)-$time < 0.01);
+    }
+
+    /** @dependsOn testLearn */
+    public function testCaseSearch() 
+    {
+        global $pdo;
+        $ac = new Autocomplete(new DB($pdo));
+        $this->assertEquals($ac->suggest('Fath'), $ac->suggest('FATH'));
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use crodas\Autocomplete\DBInterface;
+
+class DB implements DBInterface
+{
+    protected $pdo;
+    protected $prep;
+
+    public function __construct (PDO $pdo)
+    {
+        $this->pdo  = $pdo;
+        $this->prep = $pdo->prepare("REPLACE INTO autocomplete VALUES(?, ?, ?)");
+    }
+
+    public function save($text, Array $ngrams, $weight)
+    {
+        foreach ($ngrams as $ngram) {
+            $this->prep->execute([$text, $ngram, $weight]);
+        }
+    }
+
+    public function get($text)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM autocomplete WHERE ngram = ? LIMIT 10");
+        $stmt->execute([$text]);
+        return $stmt->fetchAll();
+    }
+}
+
+$pdo = new PDO('sqlite:' . __DIR__ . '/data.db');
+try {
+    $pdo->exec("CREATE TABLE autocomplete (word varchar(50), ngram varchar(10), weight)");
+    $pdo->exec("CREATE UNIQUE INDEX filter ON autocomplete (word, ngram)");
+    $pdo->exec("CREATE INDEX sort ON autocomplete (ngram, weight DESC)");
+} catch (\Exception $e) {
+}
